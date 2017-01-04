fbcfbceeb582fdc78ab01fb76b6f6911c9e67a2a
michaelh818 <michaelharootoonyan@gmail.com>
2017-01-03 21:08:05 -0800

    mongoDB how to query

--- mongoDB/insert/insert.js
+++ mongoDB/insert/insert.js
@@ -47,3 +47,4 @@ function createSomeUsers(db) {
 	});
 
 }
+
---
+++ mongoDB/query/find.js
@@ -0,0 +1,43 @@
+// Require/Import mongodb native drivers.
+var mongodb     = require('mongodb');  
+
+
+// MongoClient interface allows connection to a mongodb server.
+var MongoClient = mongodb.MongoClient; 
+
+
+// url = Connection URL.  This is where your mongodb server is running.
+var url		    = 'mongodb://localhost:27017/my_database_name';
+
+
+// Use connect method to connect to the mongodb server.
+MongoClient.connect(url, function (err, db) {
+	if (err) {
+		console.log('Unable to connect to the mongoDB server. ERROR:', err);
+	} else {
+		console.log("Connection established to %s", url)
+
+		// Finding data on mongoDB server.
+		findAllUsers(db);
+
+		db.close(); // Close connection.
+	}
+});
+
+
+function findAllUsers(db) {
+	// Get the documents collection.  If one does not exist, then it will be created on the first insert.
+	var collection = db.collection('users');
+
+	// Find collection of 'users' whos age is 22
+    var query   = { "age": 22 };
+    
+    collection.find(query).toArray(function(err, items) {
+          if (err) {
+            reject(err);
+          } else {
+            console.log(items);
+          }          
+        });
+}
+
