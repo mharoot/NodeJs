50cecbab96bb27bf3e93fe5f01f4891fc91f5ff6
michaelh818 <michaelharootoonyan@gmail.com>
2017-01-07 20:45:43 -0800

    fixed errors

--- mongoDB/projects/appendDataToHtmlTable/public/js/main.js
+++ mongoDB/projects/appendDataToHtmlTable/public/js/main.js
@@ -11,23 +11,19 @@ $(function () { // setting up the function like this assures that it runs after 
     socket.emit('get data');		
 
     socket.on('set data', function (data) {
-        if(data.length == 0)
-            socket.emit('get data');
-        else {
-            $data_table.append(table_headers);
-            for (var i = 0; i < data.length; i++) {
-                table_rows += '<tr> <td>'+data[i]._id +'</td>'
-                                  +'<td>'+data[i].name+'</td>'
-                                  +'<td>'+data[i].age +'</td>';
-                table_rows += '<td><ul>';
-                for (var j = 0; j < data[i].roles.length; j++) { // iterate through different roles.
-                    table_rows += '<li>'+data[i].roles[j]+'</li>';
-                }
-                table_rows += '</ul></td> </tr>';
+        $data_table.append(table_headers);
+        for (var i = 0; i < data.length; i++) {
+            table_rows += '<tr> <td>'+data[i]._id +'</td>'
+                                +'<td>'+data[i].name+'</td>'
+                                +'<td>'+data[i].age +'</td>';
+            table_rows += '<td><ul>';
+            for (var j = 0; j < data[i].roles.length; j++) { // iterate through different roles.
+                table_rows += '<li>'+data[i].roles[j]+'</li>';
             }
-
-            $data_table.append(table_rows);
+            table_rows += '</ul></td> </tr>';
         }
+
+        $data_table.append(table_rows);
 
     });
 
