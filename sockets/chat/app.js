0df1155cd37670f2705c305c50e9f68f4cb5afa2
Michael Harootoonyan <michaelharootoonyan@gmail.com>
2017-05-20 01:56:32 -0700

    working with rooms in nodejs using socketio

--- sockets/chat/app.js
+++ sockets/chat/app.js
@@ -1,9 +1,10 @@
-var express = require('express');
-var app     = express();
-var server  = require('http').createServer(app);
-var io      = require('socket.io').listen(server);
-users       = [];
-connections = [];
+var express     = require('express');
+var app         = express();
+var server      = require('http').createServer(app);
+var io          = require('socket.io').listen(server);
+var users       = []; // holds all user names
+var connections = []; // holds all user socketed connections
+var rooms       = []; // holds all room names and is to be implemented like a stack
 server.listen(process.env.PORT || 3000);
 
 app.get('/', function (req, res) {
@@ -11,21 +12,36 @@ app.get('/', function (req, res) {
 });
 
 io.sockets.on('connection', function (socket) {
-    connections.push(socket);
-    console.log('Connected: %s sockets connected', connections.length);
+    var nConnections   = connections.length + 1;
+    var noRoomsCreated = rooms.length == 0; 
+    var roomIsFull     = nConnections % 5 == 0 && nConnections > 4;
+
+
+    if (noRoomsCreated) 
+        rooms.push(0); // push room 0
+    else if (roomIsFull)
+        rooms.splice(0,0,rooms.length); // add a new room
+    
+    var roomName = rooms[0]; // using splice to add the rest of the rooms 
+                             // allows me to peek like this.
+
+    socket.join(roomName);    // join the socket into the room called #
+    connections.push(socket); // pushing socket after the joining to room.
+
 
     // Disconnect
     socket.on('disconnect', function (data) {
         users.splice(users.indexOf(socket.username));
         connections.splice(connections.indexOf(socket), 1);
         updateUsernames();
-        console.log('Disconnected: %s sockets connected', connections.length);
+        console.log('Disconnected: %s sockets connected', nConnections);
     });
 
 
     // Send Message
     socket.on('send message', function (data) {
-        io.sockets.emit('new message', {msg: data, user: socket.username} );
+        //io.sockets.emit('new message', {msg: data, user: socket.username} );
+        io.sockets.in(roomName).emit('new message', {msg: data, user: socket.username} );
     });
 
     // New user
@@ -36,8 +52,23 @@ io.sockets.on('connection', function (socket) {
         updateUsernames();
     });
 
+
+    // note this  function only works for the last subset for each and 
+    // every new room.
     function updateUsernames() {
-        io.sockets.emit('get users', users);
+        //io.sockets.emit('get users', users);
+
+        if (nConnections > 4) {
+            
+            var nUsersInRoom      = nConnections%4;
+            var startPos          = nConnections - nUsersInRoom;
+            var tempUsers         = users;
+            var subsetOfUsers = tempUsers.splice(startPos, nUsersInRoom);
+            io.sockets.in(roomName).emit('get users', subsetOfUsers);
+            newRoomFull = false;
+            
+        } else
+            io.sockets.in(roomName).emit('get users', users);
     }
 });
 
